80c651f49578716eee9f75ad2b481e0d0158c050
chima2g <chima2g@hotmail.com>
2021-05-04 22:15:23 +0100

    group getPowerHeatLoss tests

--- src/HouseSummary.test.js
+++ src/HouseSummary.test.js
@@ -11,17 +11,19 @@ let location = [
   },
 ];
 
-let heatLoss = 16412.5;
+describe("getPowerHeatLoss", () => {
+  let heatLoss = 16412.5;
 
-test("getPowerHeatLoss to return NaN when passed undefined location", () => {
-  expect(Number.isNaN(getPowerHeatLoss(undefined, heatLoss))).toEqual(true);
-});
-test("getPowerHeatLoss to return NaN when passed undefined heatLoss", () => {
-  expect(Number.isNaN(getPowerHeatLoss(location, undefined))).toEqual(true);
-});
-test("getPowerHeatLoss to return NaN when passed undefined location and undefined heatLoss", () => {
-  expect(Number.isNaN(getPowerHeatLoss(location, undefined))).toEqual(true);
-});
-test("getPowerHeatLoss to calculate power when passed valid location and heatLoss", () => {
-  expect(getPowerHeatLoss(location, heatLoss)).toEqual(8.944141689373296);
+  test("getPowerHeatLoss returns NaN when passed undefined location", () => {
+    expect(Number.isNaN(getPowerHeatLoss(undefined, heatLoss))).toEqual(true);
+  });
+  test("getPowerHeatLoss returns NaN when passed undefined heatLoss", () => {
+    expect(Number.isNaN(getPowerHeatLoss(location, undefined))).toEqual(true);
+  });
+  test("getPowerHeatLoss returns NaN when passed undefined location and undefined heatLoss", () => {
+    expect(Number.isNaN(getPowerHeatLoss(location, undefined))).toEqual(true);
+  });
+  test("getPowerHeatLoss calculates powerHeatLoss when passed valid location and heatLoss", () => {
+    expect(getPowerHeatLoss(location, heatLoss)).toEqual(8.944141689373296);
+  });
 });
