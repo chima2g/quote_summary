0c6e7d2e69ead282cd54a19777121bd5d320f483
chima2g <chima2g@hotmail.com>
2021-05-04 20:33:55 +0100

    output heat loss

--- src/HouseSummary.js
+++ src/HouseSummary.js
@@ -1,13 +1,22 @@
 import useFetch from "./useFetch";
 
 const HouseSummary = ({ house }) => {
+  const heatLoss =
+    house.floorArea * house.heatingFactor * house.insulationFactor;
+
   const { data: location, error } = useFetch(
     `http://localhost:8000/weather?location=${house.designRegion}`
   );
 
   return (
     <div>
-      <div>{house.designRegion}</div>
+      --------------------------------------
+      <br />
+      {house.submissionId}
+      <br />
+      --------------------------------------
+      <br />
+      Estimated Heat Loss = {heatLoss} (kWh)
       <div>
         {error && <div>Warning: Could not find design region</div>}
         {!error && location && <div>Insert required data here</div>}
